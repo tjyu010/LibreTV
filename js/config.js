2be75a7032e6aa5a16c04268c300719b7edccf96
tjyu010 <62242680+tjyu010@users.noreply.github.com>
2025-06-06 11:46:41 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -158,25 +158,25 @@ const API_SITES = {
     //     adult: true,
     //     detail: 'https://hsckzy.vip'
     // },
-    // yutu: {
-    //     api: 'https://yutuzy10.com',
-    //     name: '玉兔资源',
-    //     adult: true
-    // },
+      yutu: {
+           api: 'https://apiyutu.com/api.php/Seacms/vod',
+           name: '玉兔资源',
+           adult: true
+      },
 
     // 下面是资源失效率高的API源，不建议使用
-    // subo: {
-    //     api: 'https://subocaiji.com/api.php/provide/vod',
-    //     name: '速播资源'
-    // },
-    // fczy: {
-    //     api: 'https://api.fczy888.me/api.php/provide/vod',
-    //     name: '蜂巢资源'
-    // },
-    // ukzy: {
-    //     api: 'https://api.ukuapi88.com/api.php/provide/vod',
-    //     name: 'U酷资源'
-    // },
+      subo: {
+           api: 'https://subocaiji.com/api.php/provide/vod',
+           name: '速播资源'
+      },
+       fczy: {
+          api: 'https://api.fczy888.me/api.php/provide/vod',
+           name: '蜂巢资源'
+       },
+       ukzy: {
+           api: 'https://api.ukuapi88.com/api.php/provide/vod',
+           name: 'U酷资源'
+       },
 };
 
 // 添加聚合搜索的配置选项
